9808a89b7389ab2d7537080b3de9ffdbdb70dc3f
Dandariene <dandariene@hotmail.com>
2022-06-09 15:18:05 -0300

    Utilizando ternário no contador

--- bootcamp-spread/Javascript/Contador/assets/js/script.js
+++ bootcamp-spread/Javascript/Contador/assets/js/script.js
@@ -4,20 +4,17 @@ var currentNumber = 0
 
 function increment() {
 
-    if (currentNumber >= 20) {
-        currentNumber.innerHTML = window.alert("Você chegou ao limite da contagem")
-    } else {
-        currentNumber = currentNumber + 1
-        currentNumberWrapper.innerHTML = currentNumber
-    }
+    (currentNumber >= 20) 
+    ? currentNumber.innerHTML = window.alert("Você chegou ao limite da contagem")
+    : currentNumber = currentNumber + 1
+    currentNumberWrapper.innerHTML = currentNumber
+   
 }
 
 function decrement() {
-    if (currentNumber <= 0) {
-        currentNumber.innerHTML = window.alert("Você chegou ao limite da contagem")
-    } else {
-        currentNumber = currentNumber - 1
-        currentNumberWrapper.innerHTML = currentNumber
-    }
+    (currentNumber <= 0) 
+    ? currentNumber.innerHTML = window.alert("Você chegou ao limite da contagem")
+    : currentNumber = currentNumber - 1
+    currentNumberWrapper.innerHTML = currentNumber
 }
 
