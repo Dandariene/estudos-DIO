c59af4d1a1235882a9648d473e8cbf229c77f17e
Dandariene <dandariene@hotmail.com>
2022-06-21 12:45:04 -0300

    Estudando módulos em javascript

---
+++ bootcamp-spread/Javascript/modulos-js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs"
+
+console.log(mostraIdade('Maria', 25))
+console.log(mostraCidade('Ana', 'Ubatuba'))
+console.log(mostraHobby('Daniel', 'Jogar basquete'))
