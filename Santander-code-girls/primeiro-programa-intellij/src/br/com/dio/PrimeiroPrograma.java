a55f1f3b80c773666863455e9869158d8fd58f62
Dandariene <dandariene@hotmail.com>
2022-07-09 20:43:45 -0300

    Criando class Livro

--- Santander-code-girls/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ Santander-code-girls/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,45 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro = new Livro("A sutil arte de ligar o foda-se", 300);
+
+        System.out.println(livro);
+
 //        System.out.println("Hello World!!!");
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
