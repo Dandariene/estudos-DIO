66e16637d24dfc5abf6b613379f03f30e35b5c16
Dandariene <dandariene@hotmail.com>
2022-07-14 20:22:23 -0300

    Entendendo o uso de sobrecarga de métodos

---
+++ Santander-code-girls/primeiro-programa-intellij/src/br/com/dio/sobrecarga/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio.sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Exercício");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 6);
+        Quadrilatero.area(3,5,8);
+        Quadrilatero.area(3,2);
+    }
+}
